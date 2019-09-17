88b20fd6247a19db676abe6eb26c0bb1f89441c0
Killeg <54740006+Killeg@users.noreply.github.com>
2019-09-17 18:21:01 +0200

    Added Darkmode

--- js/script.js
+++ js/script.js
@@ -18,9 +18,28 @@ function toggleNav() {
         navDiv.classList.remove('navClosed');
         navIcon.classList.remove("fa-bars")
         navIcon.classList.add("fa-times")
-        navText.innerHTML += '<p onclick="closeNav();"class="navText"><a href="#AboutMe">About me</a></p><p onclick="closeNav();" class="navText"><a href="#Photography">Photography</a></p><p onclick="closeNav();" class="navText"><a href="#Cars">Cars</a></p>';
+        navText.innerHTML += '<p onclick="closeNav();"class="navText"><a href="#AboutMe">About me</a></p><p onclick="closeNav();" class="navText"><a href="#Photography">Photography</a></p><p onclick="closeNav();" class="navText"><a href="#Cars">Cars</a></p><label class="switch"><input type="checkbox" id="btnDarkmode" onclick="darkMode()"><span class="slider round"></span></label><p class="darkmodeText">Enable Darkmode</p>';
+        
+        if(document.body.style.background == "rgb(34, 34, 34)"){
+            document.getElementById("btnDarkmode").checked = "true";
+        }
 
     } else {
         closeNav();
+    }
+}
+
+function darkMode(){
+    var dmCheckBox = document.getElementById("btnDarkmode");
+    var htmlBodyStyle = document.body.style;
+
+    if (dmCheckBox.checked){
+        htmlBodyStyle.background = "#222";
+        htmlBodyStyle.color = "#DDD";
+    }
+
+    else{
+        htmlBodyStyle.background = "#FFF";
+        htmlBodyStyle.color = "#222";
     }
 }
